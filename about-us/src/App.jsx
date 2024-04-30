6880242269e22e257b385523f7bf1d75979fca02
FelliscaVeren <felvern11@gmail.com>
2024-04-30 22:59:34 +0700

    main

--- about-us/src/App.jsx
+++ about-us/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
-      <h1>Hello guys jadi gini</h1>
-     
+      
+      <h1>About us</h1>
+      
     </>
   )
 }
